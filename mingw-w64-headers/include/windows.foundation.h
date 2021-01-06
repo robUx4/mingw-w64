1e142df95afc696fdd7d46732c9a3369188bf812
Steve Lhomme <robux4@ycbcr.xyz>
2021-01-06 07:54:00 +0100

    SQUASH WIP headers: add C++ template helpers

--- mingw-w64-headers/include/windows.foundation.h
+++ mingw-w64-headers/include/windows.foundation.h
@@ -245,6 +245,51 @@ typedef interface __FIAsyncOperationCompletedHandler_1_UINT32 __FIAsyncOperation
 extern "C" {
 #endif
 
+#ifdef __cplusplus
+} /* extern "C" */
+// metafunctions for handling Interface Groups and Runtime Types in parameterized interfaces
+namespace ABI {
+namespace Windows { namespace Foundation { namespace Internal
+{
+    // LogicalType - the Windows Runtime type (eg, runtime class, inteface group, etc)
+    //               being provided as an argument to an _impl template, when that type
+    //               cannot be represented at the ABI.
+    // AbiType     - the type used for marshalling, ie "at the ABI", for the logical type.
+    template <class LogicalType, class AbiType>
+    struct AggregateType
+    {};
+
+    // Gets the ABI type.  See AggregateType for description.
+    template <class T>
+    struct GetAbiType
+    {
+        typedef T type;
+    };
+
+    template <class L, class A>
+    struct GetAbiType<AggregateType<L, A> >
+    {
+        typedef A type;
+    };
+
+    // Gets the LogicalType.  See AggregateType for description.
+    template <class T>
+    struct GetLogicalType 
+    {
+        typedef T type; 
+    };
+
+    template <class L, class A>
+    struct GetLogicalType<AggregateType<L, A> >
+    {
+        typedef L type;
+    };
+
+}}} // namespace Windows::Foundation::Internal
+} // ABI
+extern "C" {
+#endif // __cplusplus
+
 typedef struct EventRegistrationToken {
     UINT64 Value;
 } EventRegistrationToken;
